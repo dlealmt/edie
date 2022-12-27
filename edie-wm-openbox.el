9845d54d90ccb32c6e48c5306cc99be614f0b5c6
David Leal <dleal@mojotech.com>
2022-12-27 18:33:21 +0000

    [edie-wm-openbox] Generate rc.xml

--- edie-wm-openbox.el
+++ edie-wm-openbox.el
@@ -36,6 +36,11 @@
   "Path to Openbox's theme file."
   :type 'file)
 
+(defcustom edie-wm-openbox-rc-filename
+  (file-name-concat (xdg-config-home) "openbox" "rc.xml")
+  "Path to Openbox's rc file."
+  :type 'file)
+
 (defcustom edie-wm-openbox-theme-alist
   '(("window.handle.width" . 0)
     ("border.width" . edie-wm-window-border-width)
@@ -43,6 +48,37 @@
     ("window.inactive.border.color" . edie-wm-window-inactive-border-color))
   "Alist with option names and where to get their values."
   :type '(alist :key-type string :value-type (choice number symbol)))
+
+(defcustom edie-wm-openbox-base-rc
+  '(openbox_config (:xmlns "http://openbox.org/3.4/rc" :xmlns:xi "http://www.w3.org/2001/XInclude")
+     (focus
+       (focusNew nil)
+       (focusLast nil)
+       (followMouse nil))
+     (placement
+       (policy "Smart"))
+     (theme
+       (name "Edie")
+       (keepBorder t)
+       (animateIconify nil))
+     (desktops
+       (number 1)
+       (firstdesk 1)
+       (popupTime 0))
+     (resize
+       (drawContents t)
+       (popupShow "Never"))
+     (keyboard
+       (rebindOnMappingNotify nil))
+     (mouse
+       (screenEdgeWarpTime 0)
+       (screenEdgeWarpMouse nil))
+     (applications
+       (application (:class "*")
+         (decor nil)
+         (focus t))))
+  "Base Openbox config."
+  :type 'sexp)
 
 (defvar edie-wm-openbox--process nil)
 
@@ -92,8 +128,102 @@
     (save-buffer)
     (kill-buffer)))
 
+(defun edie-wm-openbox--mod-description (mod)
+  (pcase mod
+    ("s" "W")
+    ("M" "A")
+    ("control" "C")
+    ((or "S" "C") mod)
+    (unknown (error "Unknown modifier: %S" mod))))
+
+(defun edie-wm-openbox--char-description (char )
+  (pcase char
+    (";" "semicolon")
+    ("<c-i>" "C-i")
+    (_ char)))
+
+(defun edie-wm-openbox--key-description (key)
+  ""
+  (rx-let ((modifier (or "control" "s" "C" "M" "S")))
+    (pcase (single-key-description key)
+      ((rx string-start
+           (let mod1 modifier) "-"
+           (? "<")
+           (opt (let mod2 modifier) "-")
+           (? "<")
+           (opt (let mod3 modifier) "-")
+           (? "<")
+           (opt (let mod4 modifier) "-")
+           (let char anything)
+           (? ">")
+           string-end)
+       (concat
+        (edie-wm-openbox--mod-description mod1) "-"
+        (if mod2 (concat (edie-wm-openbox--mod-description mod2) "-") "")
+        (if mod3 (concat (edie-wm-openbox--mod-description mod3) "-") "")
+        (if mod4 (concat (edie-wm-openbox--mod-description mod4) "-") "")
+        (edie-wm-openbox--char-description char)))
+      (unknown (error "unknown key description: %S" unknown)))))
+
+(defun edie-wm-openbox--rc-keybindings ()
+  ""
+  (let ((bindings nil))
+    (map-keymap (lambda (key def)
+                  (let ((desc (single-key-description key))
+                        (ob-desc (edie-wm-openbox--key-description key)))
+                    (push
+                     `(keybind (:key ,ob-desc)
+                        (action (:name "Execute")
+                          (command ,(xml-escape-string
+                                     (format
+                                      "emacsclient --eval '(edie-keys-dispatch \"%s\")'"
+                                      desc)))))
+                     bindings)))
+                edie-keys-mode-map)
+    bindings))
+
+(defun edie-wm-openbox--rc ()
+  ""
+  (pcase-let (((seq 'openbox_config attrs &rest children) edie-wm-openbox-base-rc))
+    (append (list 'openbox_config attrs)
+            (seq-map (lambda (child)
+                       (pcase child
+                         ((seq 'keyboard &rest kchildren)
+                          (append '(keyboard) kchildren (edie-wm-openbox--rc-keybindings)))
+                         (_ child)))
+                     children))))
+
+(defun edie-wm-openbox--to-dom (tree)
+  ""
+  (pcase tree
+    (`(,tag nil)
+     (dom-node tag nil "no"))
+    (`(,tag t)
+     (dom-node tag nil "yes"))
+    (`(,tag ,(and text (pred stringp)))
+     (dom-node tag nil text))
+    (`(,tag ,(and num (pred numberp)))
+     (dom-node tag nil (format "%d" num)))
+    ((seq tag (and attrs (guard (keywordp (car attrs)))) &rest children)
+     (apply #'dom-node
+            tag
+            (map-apply (lambda (k v) (cons (seq-rest (symbol-name k)) v)) attrs)
+            (seq-map #'edie-wm-openbox--to-dom children)))
+    ((seq tag &rest children)
+     (apply #'dom-node tag nil (seq-map #'edie-wm-openbox--to-dom children)))))
+
+(defun edie-wm-openbox--write-rcxml ()
+  ""
+  (with-temp-buffer
+    (insert "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\n")
+    (dom-print (edie-wm-openbox--to-dom (edie-wm-openbox--rc)) t t)
+    (set-visited-file-name edie-wm-openbox-rc-filename)
+    (set-buffer-modified-p t)
+    (save-buffer)))
+
 (defun edie-wm-openbox--write-configuration ()
   ""
+  (edie-wm-openbox--write-rcxml)
   (edie-wm-openbox--write-theme))
 
 (defun edie-wm-openbox-reconfigure ()
