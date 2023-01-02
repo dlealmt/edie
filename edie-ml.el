96552f0af2c08122823b24d3bd0937f8ffbce8b6
David Leal <dleal@mojotech.com>
2023-01-02 23:21:15 +0000

    [edie-bar-vertico] Un-hardcode a few things

--- edie-ml.el
+++ edie-ml.el
@@ -88,39 +88,52 @@
 (cl-defgeneric edie-ml-parse (node))
 
 ;; text
+
+(defconst pt-to-pixel-ratio 1.3333343412075)
+
 (cl-defmethod edie-ml-parse (((_ (&key width height) body) (head text)))
   (let ((tspans nil)
         (rects nil))
     (pcase-dolist (`(,from ,to ,(map face)) (object-intervals body))
-      (let* ((str (substring-no-properties body from to)))
-        (if face
-            (let (fg bg fc)
-              (if (listp face)
-                  (while (and (setq fc (pop face)) (not fg) (not bg))
-                    (setq fg (or fg (face-foreground fc)))
-                    (setq bg (or bg (face-background fc))))
-                (setq fg (face-foreground face))
-                (setq bg (face-background face)))
-              (if fg
-                  (push `(tspan ((fill . ,fg)) ,str) tspans)
-                (push str tspans))
-              (when bg
-                (push `(rect ((x . ,(* from 10.5))
-                              (y . 0)
-                              (width . ,(format "%dpx" (* (- to from) 10.5)))
-                              (height . "100%")
-                              (fill . ,bg)))
-                      rects)))
-          (push str tspans))))
+      (let ((faces (cond
+                    ((listp face) (nreverse face))
+                    (face (list face))
+                    (t nil)))
+            (str (substring-no-properties body from to))
+            (text-attrs nil)
+            (rect-attrs nil))
+        (dolist (f (append faces '(default)))
+          (map-let (fill font-family font-size) text-attrs
+            (when-let (((not fill))
+                       (fg (face-attribute-specified-or (face-attribute f :foreground) nil)))
+              (push `(fill . ,fg) text-attrs))
+            (when-let (((not font-family))
+                       (fam (face-attribute-specified-or (face-attribute f :family) nil)))
+              (push `(font-family . ,fam) text-attrs))
+            (when-let (((not font-size))
+                       (size (face-attribute-specified-or (face-attribute f :height) nil)))
+              (push `(font-size . ,(format "%fpt" (/ size 10.0))) text-attrs))))
+        (dolist (f faces)
+          (map-let (fill) rect-attrs
+            (when-let (((not fill))
+                       (fg (face-attribute-specified-or (face-attribute f :background) nil)))
+              (push `(fill . ,fg) rect-attrs))))
+        (push `(tspan ,text-attrs ,str) tspans)
+        (when rect-attrs
+          (push `(rect ,(map-merge 'alist
+                                   `((x . ,(* from edie-ml-unit-x))
+                                     (y . 0)
+                                     (width . ,(format "%dpx" (* (- to from) edie-ml-unit-x)))
+                                     (height . "100%"))
+                                   rect-attrs))
+                rects))))
     (append
      (nreverse rects)
      (list `(text ((width . ,(format "%dpx" width))
                    (height . ,(format "%dpx" height))
                    (x . 0)
                    (y . "50%")
-                   ("xml:space" . "preserve")
-                   (font-family . "Ubuntu Mono")
-                   (font-size . "21px"))
+                   ("xml:space" . "preserve"))
                   ,@(nreverse tspans))))))
 
 (provide 'edie-ml)
